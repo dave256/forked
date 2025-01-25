70af509e426a181d1e68e3caf8abc9d0686ebba5
Dave Reed <dave256@mac.com>
2025-01-25 18:37:44 -0500

    can't get an array to merge changes as I expect

--- Tests/ForkedTests/ArrayOfIdentifiableMerger.swift
+++ Tests/ForkedTests/ArrayOfIdentifiableMerger.swift
@@ -83,3 +83,127 @@ struct ArrayOfIdentifiableMergerSuite {
         #expect(merged[2].value.value == 2)
     }
 }
+
+
+//extension MergeableValue: Identifiable where T: Identifiable, T.ID == UUID {
+//    public var id: UUID { value.id }
+//}
+
+@ForkedModel
+struct IntWithID: Identifiable, Equatable, Sendable {
+    var id: String = ""
+    @Merged var intValue: MergeableValue<Int> = .init(0)
+}
+
+extension Array where Element == Int {
+    var intIDArray: [IntWithID] {
+        map { .init(id: "\($0)", intValue: .init($0)) }
+    }
+}
+
+struct ConfusionAboutHowMergeWorks {
+
+    @ForkedModel
+    struct ItemArray: Equatable {
+        @Merged(using: .arrayOfIdentifiableMerge) var items: [IntWithID] = []
+    }
+
+    @Observable
+    @MainActor
+    final class Store {
+        typealias RepoType = AtomicRepository<ItemArray>
+        let repo: RepoType
+        let forkedModel: ForkedResource<RepoType>
+
+        init() throws {
+            repo =  AtomicRepository()
+            forkedModel = try ForkedResource(repository: repo)
+        }
+    }
+
+    @Test func thisPasses() async throws {
+        let store = try await Store()
+        let model = store.forkedModel
+
+        let items = ItemArray(items: [0, 0, 0].intIDArray)
+        try model.update(.main, with: items)
+
+        let f1 = Fork(name: "f1")
+        try model.create(f1)
+        let f2 = Fork(name: "f2")
+        try model.create(f2)
+
+        try model.syncAllForks()
+
+        var f1Items = try model.resource(of: f1)!
+        #expect(f1Items.items.count == 3)
+        #expect(f1Items.items.map { $0.intValue.value } == [0, 0, 0])
+
+        f1Items.items[1].intValue.value = 1
+        #expect(f1Items.items.map { $0.intValue.value } == [0, 1, 0])
+        try model.update(f1, with: f1Items)
+
+        try model.syncAllForks()
+
+        var f2Items = try model.resource(of: f2)!
+        #expect(f2Items.items.count == 3)
+        #expect(f2Items.items.map { $0.intValue.value } == [0, 1, 0])
+
+        f2Items.items[2].intValue.value = 2
+        try model.update(f2, with: f2Items)
+
+        try model.syncAllForks()
+
+        let updatedItems: ItemArray
+        updatedItems = try model.resource(of: .main)!
+        #expect(updatedItems.items.map { $0.intValue.value } == [0, 1, 2])
+    }
+
+    @Test func thisFails() async throws {
+        let store = try await Store()
+        let model = store.forkedModel
+
+        let items = ItemArray(items: [0, 0, 0].intIDArray)
+        try model.update(.main, with: items)
+
+        let f1 = Fork(name: "f1")
+        try model.create(f1)
+        let f2 = Fork(name: "f2")
+        try model.create(f2)
+        try model.syncAllForks()
+
+        // both f1Items and f2Items start with [0, 0, 0] from main
+        var f1Items = try model.resource(of: f1)!
+        #expect(f1Items.items.count == 3)
+        #expect(f1Items.items.map { $0.intValue.value } == [0, 0, 0])
+
+        var f2Items = try model.resource(of: f2)!
+        #expect(f2Items.items.count == 3)
+        #expect(f2Items.items.map { $0.intValue.value } == [0, 0, 0])
+
+        // f1 is updated to [0, 1, 0]
+        f1Items.items[1].intValue.value = 1
+        #expect(f1Items.items.map { $0.intValue.value } == [0, 1, 0])
+        try model.update(f1, with: f1Items)
+        // merge this change into main
+        try model.mergeIntoMain(from: f1)
+
+        var updatedItems: ItemArray
+        // now main has [0, 1, 0]
+        updatedItems = try model.resource(of: .main)!
+        #expect(updatedItems.items.map { $0.intValue.value } == [0, 1, 0])
+
+        // f2 is updated to [0, 0, 2]
+        f2Items.items[2].intValue.value = 2
+        #expect(f2Items.items.map { $0.intValue.value } == [0, 0, 2])
+        try model.update(f2, with: f2Items)
+        // merge this change into main
+        try model.mergeIntoMain(from: f2)
+
+        // why doesn't main now have [0, 1, 2]
+        updatedItems = try model.resource(of: .main)!
+        #expect(updatedItems.items.map { $0.intValue.value } == [0, 1, 2])
+    }
+
+
+}
